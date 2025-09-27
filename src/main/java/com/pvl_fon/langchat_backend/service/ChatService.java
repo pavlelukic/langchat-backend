724f2ad5fa4c5d90cb29a628394288584d617f6d
pavlelukic <pavlelukic2015@gmail.com>
2025-09-27 20:28:10 +0200

    Marked the ChatService with @Service and removed unused imports.

--- src/main/java/com/pvl_fon/langchat_backend/service/ChatService.java
+++ src/main/java/com/pvl_fon/langchat_backend/service/ChatService.java
@@ -1,18 +1,18 @@
 package com.pvl_fon.langchat_backend.service;
 
 import com.pvl_fon.langchat_backend.dto.ChatRequest;
-import dev.langchain4j.data.message.AiMessage;
 import dev.langchain4j.data.message.ChatMessage;
 import dev.langchain4j.data.message.SystemMessage;
 import dev.langchain4j.data.message.UserMessage;
 import dev.langchain4j.model.chat.response.ChatResponse;
 import dev.langchain4j.model.openai.OpenAiChatModel;
 import dev.langchain4j.model.chat.ChatModel;
-import dev.langchain4j.model.output.Response;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class ChatService {
     public String chat(ChatRequest request){
         String apiKey = System.getenv("OPENAI_API_KEY");
