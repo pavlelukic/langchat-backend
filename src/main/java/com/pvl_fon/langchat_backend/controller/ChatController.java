cb6dfda91940a491607a6b86f58f8832c0d37eb6
pavlelukic <pavlelukic2015@gmail.com>
2025-09-27 23:46:02 +0200

    Added support for Slf4j logging. Logged the steps to make sure everything goes as planned. Added a try-catch block for error handling.

--- src/main/java/com/pvl_fon/langchat_backend/controller/ChatController.java
+++ src/main/java/com/pvl_fon/langchat_backend/controller/ChatController.java
@@ -3,11 +3,13 @@ package com.pvl_fon.langchat_backend.controller;
 import com.pvl_fon.langchat_backend.dto.ChatRequest;
 import com.pvl_fon.langchat_backend.dto.ChatResponse;
 import com.pvl_fon.langchat_backend.service.ChatService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/chat")
 @CrossOrigin(origins = {"http://localhost:3000", "http://localhost:5173"})
+@Slf4j
 public class ChatController {
 
     private final ChatService chatService;
@@ -18,8 +20,19 @@ public class ChatController {
 
     @PostMapping
     public ChatResponse sendMessage(@RequestBody ChatRequest request){
-        String reply = chatService.chat(request);
-        return new ChatResponse(reply);
+        log.info(("Received message: " + request.getMessage() + " at " + java.time.LocalDateTime.now());
+        log.info("Using model: " + request.getModel());
+        try{
+            log.info("Calling chat service...");
+            String reply = chatService.chat(request);
+            log.info("Reply received successfully!");
+            return new ChatResponse(reply);
+        } catch (Exception ex){
+            log.error("Error processing message: " + ex.getMessage());
+            ex.printStackTrace();
+            throw ex;
+        }
+
     }
 
 }
